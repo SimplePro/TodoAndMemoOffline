0f5e9734610ec6044c2b53123485ec7078400df2
simplepro <simplepro1254@gmail.com>
2020-07-04 12:53:31 +0900

    TodoInstance 에 프라이머리키, 컬럼 을 추가하였다. 그리고 data class 로 바꾸었다.

--- app/src/main/java/com/simplepro/todoandmemooffline/instance/TodoInstance.kt
+++ app/src/main/java/com/simplepro/todoandmemooffline/instance/TodoInstance.kt
@@ -1,3 +1,11 @@
 package com.simplepro.secondtodoandmemo.instance
 
-data class TodoInstance (val todo : String = "", val content : String = "", val todoId: String = "")
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo")
+data class TodoInstance (
+    @ColumnInfo(name = "todo") val todo : String = "",
+    @ColumnInfo(name = "content") val content : String = "",
+    @PrimaryKey val todoId: String = "")
