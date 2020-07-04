9aacaf67c6b57455e6a05314dfabd193a5d82439
simplepro <simplepro1254@gmail.com>
2020-07-04 12:52:02 +0900

    todoDao 생성

---
+++ app/src/main/java/com/simplepro/todoandmemooffline/Dao/TodoDao.kt
@@ -0,0 +1,18 @@
+package com.simplepro.todoandmemooffline.Dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.simplepro.secondtodoandmemo.instance.TodoInstance
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM todo")
+    fun getAll() : ArrayList<TodoInstance>
+
+    @Insert
+    fun insert(todo : TodoInstance)
+
+    @Query("DELETE from todo")
+    fun deleteAll()
+}
